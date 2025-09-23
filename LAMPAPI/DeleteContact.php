0461db9ada13c6fd55e2f64314f2be9eaf6b476f
Minh Thien Pham <102099064+Github065@users.noreply.github.com>
2025-09-23 17:18:21 -0400

    Update DeleteContact.php
    Removed unnecessary headers

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,8 +1,4 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-	header("Access-Control-Allow-Headers: Content-Type");
-
 	$inData = getRequestInfo();
 	
 	$id  = $inData["id"];
